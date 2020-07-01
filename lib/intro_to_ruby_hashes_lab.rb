63a930168a3d6cf55fbc80bc63a2dddb1d17d03a
Jessica  Rattani <jjrattani@gmail.com>
2020-07-01 02:37:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,14 +4,17 @@ end
 
 def my_hash
   grades = {name: "linda"}
-grades
+  grades
 end
 
 def pioneer
+  engineer = {name: "Grace Hopper"}
+  return engineer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  {id: number}
   # return a hash with a key :id assigned to the provided number
 end
